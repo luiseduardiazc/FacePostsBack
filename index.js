089eb841afb2afb9db2418477d35d1d04fddb6b6
luis <luis.diaz.car@gmail.com>
2020-06-19 14:55:38 -0500

    manage default host 0.0.0.0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const config = require('./config');
 
 const PORT = config.server.PORT || 3000;
-const HOST = config.server.SERVER_HOSTNAME || 'localhost';
+const HOST = config.server.SERVER_HOSTNAME || '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
   console.log(`api run http://${HOST}:${PORT}`);
