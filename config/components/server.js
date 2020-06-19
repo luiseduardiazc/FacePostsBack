273dffd99b1ef6a1abdc89d18c674efd43935b49
luis <luis.diaz.car@gmail.com>
2020-06-19 14:49:04 -0500

    config host env

--- config/components/server.js
+++ config/components/server.js
@@ -3,7 +3,7 @@
 const config = {
   PORT: process.env.PORT || process.env.SERVER_PORT,
   SECRET_KEY: process.env.SECRET_KEY,
-  SERVER_HOSTNAME: process.env.SERVER_HOSTNAME
+  SERVER_HOSTNAME: process.env.HOST || process.env.SERVER_HOSTNAME
 };
 
 module.exports = config;
